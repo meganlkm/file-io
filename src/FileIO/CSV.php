d2151adc8b83300d9e7881cf4c6444345892301e
meganlkm <megan.lkm@gmail.com>
2014-09-04 21:24:41 -0500

    add docblock to FileIO\CSV

--- src/FileIO/CSV.php
+++ src/FileIO/CSV.php
@@ -10,44 +10,86 @@ class CSV
     protected $header;
     protected $data;
 
+    /**
+     * do something with a csv file
+     *
+     * @param File $file File object to write/read csv data
+     */
     public function __construct(File $file)
     {
         $file->setType('csv');
         $this->file = $file;
     }
 
+    /**
+     * destroy File reference
+     */
     public function __destruct()
     {
         $this->file = null;
     }
 
+    /**
+     * construct a new instance and chain
+     *     other methods
+     *
+     * @param  File   $file File object to write/read csv data
+     * @return FileIO\CSV       return new instance of CSV
+     */
     public static function newInstance(File $file)
     {
         return new self($file);
     }
 
+    /**
+     * set the csv header row
+     *
+     * @param array $header
+     * @return  FileIO\CSV return this instance to support method chaining
+     */
     public function setHeader($header = array())
     {
         $this->header = $header;
         return $this;
     }
 
+    /**
+     * get the header row
+     *
+     * @return array
+     */
     public function getHeader()
     {
         return $this->header;
     }
 
+    /**
+     * set the csv data
+     *
+     * @param array $data array of arrays - each csv row is an array
+     * @return  FileIO\CSV return this instance to support method chaining
+     */
     public function setData($data = array())
     {
         $this->data = $data;
         return $this;
     }
 
+    /**
+     * get csv data
+     *
+     * @return array
+     */
     public function getData()
     {
         return $this->data;
     }
 
+    /**
+     * write the header array to this File
+     *
+     * @return  FileIO\CSV return this instance to support method chaining
+     */
     public function writeHeader()
     {
         if (!empty($this->header)) {
@@ -56,6 +98,11 @@ class CSV
         return $this;
     }
 
+    /**
+     * write the data array to this File
+     *
+     * @return  FileIO\CSV return this instance to support method chaining
+     */
     public function writeData()
     {
         if (!empty($this->data)) {
